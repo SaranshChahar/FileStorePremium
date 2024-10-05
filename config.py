be32012cca9b6b9936e9c05f79484a5a52ce1c26
SARANSH CHAHAR <154498089+SaranshChahar@users.noreply.github.com>
2024-10-05 21:22:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -31,8 +31,8 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "7385295244"))
 PORT = os.environ.get("PORT", "8030")
 
 #Database
-DB_URI = os.environ.get("DATABASE_URL", "")
-DB_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
+DB_URL = os.environ.get("DB_URL", "")
+DB_NAME = os.environ.get("DB_NAME", "Cluster0")
 
 #force sub channel id, if you want enable force sub
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", ""))
